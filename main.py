5d81665521df7f26ea8f53d9a7237d733c16c2f7
Vinh Loi Chau <chauvinhloi@gmail.com>
2019-08-04 12:15:21 +1000

    build-and-store-complete

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.neighbors import KNeighborsClassifier
+
+with open('./model.pkl', 'rb') as model_pkl:
+    knn = pickle.load(model_pkl)
+
+unseen = np.array([[3.2, 1.1, 1.5, 2.1]])
+result = knn.predict(unseen)
+
+print('Predicted result for observation ' + str(unseen) + ' is: ', result)
