01ed70d0d11f7bc2a89a58f158d4403fcfa8e91a
Vinh Loi Chau <chauvinhloi@gmail.com>
2019-08-04 12:45:15 +1000

    flask added

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import pandas as pd
 import numpy as np
 import pickle
+import sys
 from sklearn.neighbors import KNeighborsClassifier
+from flask import Flask, request
 
 with open('./model.pkl', 'rb') as model_pkl:
     knn = pickle.load(model_pkl)
 
-unseen = np.array([[3.2, 1.1, 1.5, 2.1]])
-result = knn.predict(unseen)
 
-print('Predicted result for observation ' + str(unseen) + ' is: ', result)
+app = Flask(__name__)
+
+@app.route('/predict')
+def predict_iris():
+    sl = request.args.get('sl')
+    sw = request.args.get('sw')
+    pl = request.args.get('pl')
+    pw = request.args.get('pw')
+
+    unseen = np.array([[sl, sw, pl, pw]])
+    result = knn.predict(unseen)
+
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run()
