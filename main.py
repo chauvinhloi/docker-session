1e636b11a609c12de835ad05111e7f3343cd246c
Vinh Loi Chau <chauvinhloi@gmail.com>
2019-08-04 12:58:35 +1000

    Flasgger added

--- main.py
+++ main.py
@@ -4,15 +4,46 @@ import pickle
 import sys
 from sklearn.neighbors import KNeighborsClassifier
 from flask import Flask, request
+from flasgger import Swagger
 
 with open('./model.pkl', 'rb') as model_pkl:
     knn = pickle.load(model_pkl)
 
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 @app.route('/predict')
 def predict_iris():
+    """
+    API endpoint for predicting iris species
+    ---
+    parameters:
+        - name: sl
+          description: sepal length
+          in: query
+          type: number
+          required: true
+        - name: sw
+          description: sepal width
+          in: query
+          type: number
+          required: true
+        - name: pl
+          description: petal length
+          in: query
+          type: number
+          required: true
+        - name: pw
+          description: petal width
+          in: query
+          type: number
+          required: true
+    responses:
+        '200':
+          description: OK
+
+    """
     sl = request.args.get('sl')
     sw = request.args.get('sw')
     pl = request.args.get('pl')
